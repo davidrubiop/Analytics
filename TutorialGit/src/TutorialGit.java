24268f0e48d4c280b8599834f909013d93225ec7
Juan Garcia <Juan Garcia@JuanGarcia>
2013-09-18 10:21:35 -0500

    gyghg

--- TutorialGit/src/TutorialGit.java
+++ TutorialGit/src/TutorialGit.java
@@ -6,7 +6,7 @@ public class TutorialGit {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-				System.out.println("hola mundo");
+				System.out.println("hola mundnkhjjhjhhjo");
 	}
 
 }
